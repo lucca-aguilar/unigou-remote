edf6671bf906ed2207805fa138b1f7360ed5e1f0
Lucca Aguilar <sophia.santos.aguilar@gmail.com>
2026-05-23 18:00:51 -0300

    A

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,39 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
-#include "trace.h"
+#include "logger.h"
+#include "mem_monitor.h"
+#include "runtime_stats.h"
 
-void Task1(void *pvParameters);
-void Task2(void *pvParameters);
-void TaskMonitor(void *pvParameters);
-
-TaskHandle_t TaskHandle_1;
-TaskHandle_t TaskHandle_2;
+static void Task1(void* pvParameters);
+static void Task2(void* pvParameters);
 
 void setup() {
-    Serial.begin(115200);
-
-    Serial.println("--- System Initialized ---");
-
-    trace_init();
-
-    Serial.println("Tracer initializing...");
-
-    xTaskCreate(Task1, "Task 1", 128, NULL, 1, NULL);
-    xTaskCreate(Task2, "Task 2", 128, NULL, 1, NULL);
-    xTaskCreate(TaskMonitor, "Monitor", 256, NULL, 3, NULL); // highest priority
-
-    vTaskStartScheduler();
-}
-
-void loop() {
-
-}
-
-void TaskMonitor(void *pvParameters)
-{
-    static bool printed = false;
-    while (1)
-    {
-        if (traceIndex >= MAX_TRACES && !printed)
-        {
-            Serial.println("### FULL BUFFER - EXPORTING CSV ###");
-            vTaskSuspendAll();
-            trace_print_csv();
-            printed = true;
-            xTaskResumeAll();
-            Serial.println("### END OF EXPORTATION ###");
-        }
-        vTaskDelay(pdMS_TO_TICKS(500));
+    while (!Serial) {
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
+
+    log_init();
+    mem_monitor_init();
+    runtime_stats_init();
+
+    Serial.println("BOOT");
+
+    xTaskCreate(Task1, "Task1", 192, NULL, 2, NULL);
+    xTaskCreate(Task2, "Task2", 192, NULL, 2, NULL);
 }
 
-void Task1(void *pvParameters)
-{
-    while (1)
-    {
-        trace_record("Task 1", STATE_RUNNING);
+void loop() {}
+
+static void Task1(void* pvParameters) {
+    while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
-        trace_record("Task 1", STATE_READY);
-        vTaskDelay(pdMS_TO_TICKS(100));
-        ram_record(xPortGetFreeHeapSize());
+        log_write(OPERATION, "Task1 operating.");
     }
 }
 
-void Task2(void *pvParameters)
-{
-    while (1)
-    {
-        trace_record("Task 2", STATE_RUNNING);
+static void Task2(void* pvParameters) {
+    while (1) {
         vTaskDelay(pdMS_TO_TICKS(600));
-        trace_record("Task 2", STATE_READY);
-        vTaskDelay(pdMS_TO_TICKS(100));
-        ram_record(xPortGetFreeHeapSize());
+        log_write(OPERATION, "Task2 operating.");
     }
 }
--- src/runtime_stats.cpp
+++ src/runtime_stats.cpp
@@ -2,8 +2,31 @@
 #include <Arduino_FreeRTOS.h>
 #include "runtime_stats.h"
 
-void vConfigureTimerForRunTimeStats(void) {
-    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-    DWT->CYCCNT    = 0;
-    DWT->CTRL      |= DWT_CTRL_CYCCNTENA_Msk;
+static void runtime_task(void* pvParameters);
+
+extern "C" {
+    void vConfigureTimerForRunTimeStats(void) {
+        CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+        DWT->CYCCNT    = 0;
+        DWT->CTRL      |= DWT_CTRL_CYCCNTENA_Msk;
+    }
+
+    uint32_t ulGetRunTimeCounterValue(void) {
+        return DWT->CYCCNT;
+    }
+}
+
+void runtime_stats_init() {
+    xTaskCreate(runtime_task, "Runtime Stats Task", 512, NULL, 1, NULL);
+}
+
+static void runtime_task(void* pvParameters) {
+    TickType_t LastWakeTime = xTaskGetTickCount();
+
+    while(1) {
+        vTaskDelayUntil(&LastWakeTime, pdMS_TO_TICKS(5000));
+        static char buffer[1000];
+        vTaskGetRunTimeStats(buffer);
+        Serial.print(buffer);
+    }
 }
